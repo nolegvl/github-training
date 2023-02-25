cac622f795c8104187887becfdb387e5d7f96880
olegvl <112123326+lordcraboff@users.noreply.github.com>
2023-02-25 14:06:08 +0000

    new basg features

--- hello.sh
+++ hello.sh
@@ -1,6 +1,11 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 echo 'Hello World!';
 
-
+uname -srom;
 df --output=source,pcent,used -H | grep overlay
+
+du -sh /home; du -sh ..
+
+STR=$'1. some text \n2. some text \n3. some text'
+echo "$STR"
